177bb1e44f7d3228f4e8fc87eb3de146b2720ce7
arniti-ibra <120824847+arniti-ibra@users.noreply.github.com>
2023-01-17 12:05:14 +0000

    added vars necessary for main.tf
    you could also add a variable for location with default = "West Europe" to make line 7 in main.tf parameterised

--- variables.tf
+++ variables.tf
@@ -1,9 +1,25 @@
-variable "resource_group_location" {
-  default     = "East US"
-  description = "Location of the resource group."
+variable "environment_name" {
+  default = "sbx"
 }
 
-variable "resource_group_name_prefix" {
-  default     = "rg"
-  description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
+variable "purpose" {
+  default = "tfvm"
+}
+
+variable "resource_group_prefix" {
+  default = "rg"
+}
+variable "storage_account_prefix" {
+  default = "sta"
+}
+variable "instance_id" {
+  default = "1"
+}
+
+variable "cloud_service_provider" {
+  default = "az"
+}
+
+variable "operating_system" {
+  default = "lin"
 }
